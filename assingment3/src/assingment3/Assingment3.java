7eb25ef6ec59d5b661d4dfe180411a1096761445
Jaswant Singh <jaswantnoori.com>
2023-11-30 20:54:50 -0500

    First Commit New project by Jaswant Singh

---
+++ assingment3/src/assingment3/Assingment3.java
@@ -0,0 +1,37 @@
+package assingment3;
+
+public class Assingment3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// program rolls a 20 sided dice 1000 times and records the number
+		// of occurrences of each result.
+		// See:https://en.wikipedia.org/wiki/Dice#Common_variations
+		// Math.random() returns 0.0 to 1.0 excluding 1.0
+		// i.e. we could get 0.99999999999 etc. but not 1.0
+		// so Math.random() times upper range of 20 is 0 to 19
+
+		// Hint: it is easy to make a mistake with array indexes as people
+		// (and dice) count from 1 upwards, while programmers using arrays
+		// count from 0 upwards.
+		int roll = 0;
+		int[] diceRolls = new int[20];
+		int totalRolls = 0;
+
+		// sample the rolls
+		for(int count = 0; count < 1000; count++) {
+			roll = (int)(Math.random() * 20) + 1;
+			diceRolls[roll - 1]++; // Adjust the index to avoid ArrayIndexOutOfBoundsException
+        }
+		
+		// run a report
+		for(int index = 0; index < diceRolls.length; index++) {
+			System.out.printf("Count of %d is: %d%n", (index + 1), diceRolls[index]);
+			totalRolls = totalRolls + diceRolls[index];
+		}
+		System.out.println("Total rolls were: " + totalRolls);
+		System.out.println("New Project Assignment03 Jaswant Singh");
+
+	}
+
+}
